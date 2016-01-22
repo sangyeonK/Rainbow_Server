4e60941e1fcac7529127fb5bd79cb9f588be86bb
sangyeonK <sovietist@hotmail.com>
2016-01-22 17:54:46 +0900

    req헤더에 세션값 없을경우 처리

--- Server/common/responsor.js
+++ Server/common/responsor.js
@@ -11,7 +11,7 @@ module.exports = function( err, res, result )
 {
     if( err )
     {
-        logger.error("["+ res.req.url + "]\n" + res.req.headers.rs + "\n" + JSON.stringify(res.req.body) + "\n" + err.message);
+        logger.error("["+ res.req.url + "]\n" + ( res.req.headers.rs !== undefined ? res.req.headers.rs : "" ) + "\n" + JSON.stringify(res.req.body) + "\n" + err.message);
         if( err.sqlState )
         {
             res.send( makeResponse(0,"DATABASE_ERROR",{}) );
