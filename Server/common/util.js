18b6f4f0ba9e79bf7e5b71585a3fc6ba1a422106
sangyeonK <sovietist@hotmail.com>
2016-01-23 12:42:33 +0900

    에러코드 목록 정리

--- Server/common/util.js
+++ Server/common/util.js
@@ -2,16 +2,16 @@ var mysql = require( './mysql.js' );
 
 var errorCodeList = 
 {
-    "DATABASE_ERROR" : 1,
-    "INVALID_SESSION" : 2,
-    "BAD_REQUEST" : 3,
-    "ALREADY_EXIST_ID" : 4,
-    "INVALID_ACCOUNT" : 5,
-    "INVALID_ID_PASSWORD" : 6,
-    "INVALID_GROUP" : 7,
-    "ALREADY_IN_THE_GROUP" : 8,
+    "DATABASE_ERROR" : 1,           //데이터베이스 처리중 오류 발생
+    "INVALID_SESSION" : 2,          //세션값 오류
+    "BAD_REQUEST" : 3,              //파라미터 입력 오류
+    "ALREADY_EXIST_ID" : 4,         //이미 있는 ID
+    "INVALID_ACCOUNT" : 5,          //잘못된 계정
+    "INVALID_ID_PASSWORD" : 6,      //(르그인 시)잘못된 ID 혹은 패스워드
+    "INVALID_GROUP" : 7,            //잘못된 그룹
+    "ALREADY_IN_THE_GROUP" : 8,     //이미 그룹에 참가한 사용자
     
-    "GENERAL_ERROR" : 99
+    "GENERAL_ERROR" : 99            //기타 일반적인 오류
 };
 
 module.exports.checkParameter = function( params , request_body) {
