00cdc139d080431aad8e0583cc490f9642b785c8
sangyeonK <sovietist@hotmail.com>
2016-01-25 23:47:31 +0900

    공용함수 추가작업
    request 검사 함수 추가
    unixtime 처리 함수 추가

--- Server/common/util.js
+++ Server/common/util.js
@@ -1,4 +1,6 @@
 var mysql = require( './mysql.js' );
+var responsor = require('./responsor.js');
+var auth = require('./auth.js');
 
 var errorCodeList = 
 {
@@ -10,12 +12,71 @@ var errorCodeList =
     "INVALID_ID_PASSWORD" : 6,      //(르그인 시)잘못된 ID 혹은 패스워드
     "INVALID_GROUP" : 7,            //잘못된 그룹
     "ALREADY_IN_THE_GROUP" : 8,     //이미 그룹에 참가한 사용자
+    "ALREADY_FULL_GROUP" : 9,       //이미 사용자가 가득 찬 그룹
     
     "GENERAL_ERROR" : 99            //기타 일반적인 오류
 };
 
 var inviteCodeAlphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var inviteCodeLength = 16;
+
+module.exports.checkRequest = function( req, requireParams, needSession) {
+
+    var session,inputParams,result = {};
+    
+    if(req.method == "GET")
+        inputParams = req.query;
+    else if(req.method == "POST")
+        inputParams = req.body;
+
+    //check session
+    if(req.headers['rs'] == undefined )
+    {
+        result.err = new Error("INVALID_SESSION");
+        return result;
+    }
+        
+    session = auth.decrypt( req.headers['rs'] );
+    
+    if(session == undefined)
+    {
+        result.err = new Error("INVALID_SESSION");
+        return result;
+    }
+    
+    //check parameters
+    if( inputParams == undefined || inputParams == false )
+    {
+        result.err = new Error("BAD_REQUEST");
+        return result;
+    }
+    
+    if( requireParams.length != this.objectSize(inputParams) )
+    {
+        result.err = new Error("BAD_REQUEST");
+        return result;
+    }
+    
+    for(var i = 0 ; i < requireParams.length ; i++ )
+    {
+        if(inputParams[ requireParams[i] ] == undefined )
+        {
+            result.err = new Error("BAD_REQUEST");
+            return result;
+        }
+        
+        var val = inputParams[ requireParams[i] ];
+        
+        if( /^\d+$/.test(val) )
+            val = parseInt( val );
+        else
+            val = mysql.escape( val );
+        
+        inputParams[ requireParams[i] ] = val;
+    }
+    
+    return {session:session, params:inputParams};
+};
 
 module.exports.checkParameter = function( params , request_body) {
 
@@ -30,7 +91,15 @@ module.exports.checkParameter = function( params , request_body) {
         {
             return false;
         }
-        request_body[ params[i] ] = mysql.escape( request_body[ params[i] ] );
+        
+        var val = request_body[ params[i] ];
+        
+        if( /^\d+$/.test(val) )
+            val = parseInt( val );
+        else
+            val = mysql.escape( val );
+        
+        request_body[ params[i] ] = val;
     }
     return request_body;
 };
@@ -45,6 +114,16 @@ module.exports.objectSize = function(obj) {
 
 module.exports.getUnixTime = function() {
     return Math.floor(Date.now() / 1000);
+};
+
+module.exports.makeUnixTime = function(year, month, day) { 
+    return Math.floor(new Date(year,month-1,day,0,0,0).getTime() / 1000);
+};
+
+module.exports.parseUnixTime = function(unixtime) { 
+    var d = new Date(unixtime * 1000);
+    
+    return { year:d.getFullYear(), month:d.getMonth() + 1 , day:d.getDate(), hour:d.getHours(), min:d.getMinutes(), sec:d.getSeconds() };
 };
 
 module.exports.error = function(code,message) {
