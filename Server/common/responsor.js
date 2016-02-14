986284a55ac41667b3101551c62d5348a828e956
sangyeonK <sovietist@hotmail.com>
2016-02-14 14:39:25 +0900

    api 성공/실패 상황에 따른 응답구조 차이 적용
    성공시
    {
        result ; {}
    }
    실패시
    {
        errorCode : int
        errorMessage ; string
    }

--- Server/common/responsor.js
+++ Server/common/responsor.js
@@ -13,16 +13,16 @@ module.exports = function( err, res, result )
     {
         if( err.sqlState )
         {
-            res.status(500).send( makeResponse(util.getErrorCode("DATABASE_ERROR"),"DATABASE_ERROR",{}) );
+            res.status(500).send( makeResponse(util.getErrorCode("DATABASE_ERROR"),"DATABASE_ERROR",undefined) );
         }
         else
         {
-            res.status(500).send( makeResponse(util.getErrorCode(err.message),err.message,{}) );
+            res.status(500).send( makeResponse(util.getErrorCode(err.message),err.message,undefined) );
         }
         logger.error("["+ res.req.url + "]\n" + ( res.req.headers.rs !== undefined ? res.req.headers.rs : "" ) + "\n" + JSON.stringify(res.req.body) + "\n" + ( err.sqlState !== undefined ? "1" : util.getErrorCode(err.message) ) + ' ' + err.message);
     }
     else
     {
-        res.send( makeResponse(0,"",result) );
+        res.send( makeResponse(undefined,undefined,result) );
     }
 };
